35aeb6c3d1078f3b987e273c7f85064dbe55e808
Gregory Iiams <levi@li-mbp.local>
2018-09-01 10:33:27 -0500

    add support for pushing overlay to multiple devices

--- bulkOverlayTool.py
+++ bulkOverlayTool.py
@@ -1,9 +1,11 @@
+import getpass
 import json
 import requests
 import sys
 import time
-import urllib3
 
+
+import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 def getYorN(prompt):
@@ -11,99 +13,51 @@ def getYorN(prompt):
         try:
             return {"y":True,"n":False}[input(prompt).lower()]
         except KeyError:
-            print("\nInvalid entry, please enter 'Y' or 'N'!\n")
-def camRespToRefs(cam_resp):
-    cam_name = cam_resp['name']
-    cam_uid = cam_resp['uid']
-    cam_object_type = cam_resp['objectType']
-    cam_vsom_uid = cam_resp['vsomUid']
-    cam_oper_state = cam_resp['operState']
-    cam_ref = {'camName':cam_name,'camUid':cam_uid,'objectType':cam_object_type,'refVsomUid':cam_vsom_uid,'operState':cam_oper_state}
+            print("Invalid input, please enter 'Y' or 'N'!")
 
-    return cam_ref
+def getCamByLocation(locationName):
+    start = 0
+    more_pages = True
+    locationUid = []
+    cameras = []
+    locationFilter = {"filter":{"byLocationType":"DEFAULT","byLocalNameContains":locationName}}
+    locationReq = s.post('https://{0}/ismserver/json/location/v3_1/getLocations'.format(vsmIp), json=locationFilter, verify=False)
+    locationResp = json.loads(locationReq.text)
+    for item in locationResp['data']['items']:
+        locationUid.append(item['uid'])
 
-def getCamByLocation(location_name):
-    location_uid = []
-    camera_refs = []
-    cameras_remaining = True
-    print("Grabbing UID's for location: {0}".format(location_name))
-    location_filter = {'filter':{'byLocationType':'DEFAULT','byLocalNameContains':location_name}}
-    print("Using this filter for location search: \n{0}".format(location_filter))
-    location_req = s.post('https://{0}/ismserver/json/location/v3_1/getLocations'.format(vsom_ip), json=location_filter, verify=False)
-    location_resp = json.loads(location_req.text)
-    if location_resp['data']['totalRows'] == 0:
-        print("Got no location UID's!")
-        return "Failed"
-    for item in location_resp['data']['items']:
-        if item['uid'] != '40000000-0000-0000-0000-000000000005':
-            print(item)
-            location_uid.append(item['uid'])
-
-    if len(location_uid) > 1:
-        continue_multiple_locations = getYorN(" Got more than one location. Continue? ")
-        if continue_multiple_locations == False:
-            print("Stopping! Try another spelling, or apply overlays manually.")
+    if len(locationUid) > 1:
+        keep_going = getYorN("Got more than one location. Continue?")
+        if keep_going == False:
+            print("Stopping!!!")
             return False
-        else:
-            start = 0
-            limit = 100
-            while cameras_remaining == True:
-                cam_filter = {'filter':{'byLocationUids':location_uid,'pageInfo':{'start':start,'limit':limit}}}
-                print("\n\nPaging: {0}\n\n".format(cam_filter['filter']['pageInfo']))
-                get_cameras = s.post('https://{0}/ismserver/json/camera/v3_1/getCameras'.format(vsom_ip), json=cam_filter, verify=False)
-                cam_resp = json.loads(get_cameras.text)
-                for item in cam_resp['data']['items']:
-                    camera_refs.append(camRespToRefs(item))
-                print(cam_resp['data']['nextPageExists'])
+    while more_pages == True:
+        camFilter = {"filter":{"byLocationUids":locationUid,"pageInfo":{"start":start,"limit":500}}}
+        getCameras = s.post('https://{0}/ismserver/json/camera/v3_1/getCameras'.format(vsmIp), json=camFilter, verify=False)
+        cameraResp = json.loads(getCameras.text)
+        for item in cameraResp['data']['items']:
+            cameras.append(item)
+        if cameraResp['data']['nextPageExists'] == True:
+            start += 500
+            limit += 500
+        elif cameraResp['data']['nextPageExists'] == False:
+            more_pages = False
 
-                if cam_resp['data']['nextPageExists'] == True:
-                    print("Need to get some more cameras.")
-                    start += 100
-                    limit += 100
-                    print("Still got cameras? {0}".format(cameras_remaining))
-                    time.sleep(1)
-
-                if cam_resp['data']['nextPageExists'] == False:
-                    cameras_remaining == False
-                    print("Should have all cameras now.")
-
-                    return camera_refs
-
-    if len(location_uid) == 1:
-        start = 0
-        limit = 100
-        times_run = 0
-        print("Just one location.")
-        while cameras_remaining == True:
-            times_run += 1
-            print(times_run)
-            cam_filter = {'filter':{'byLocationUids':location_uid,'pageInfo':{'start':start,'limit':limit}}}
-            get_cameras = s.post('https://{0}/ismserver/json/camera/v3_1/getCameras'.format(vsom_ip), json=cam_filter, verify=False)
-            cam_resp = json.loads(get_cameras.text)
-            for item in cam_resp['data']['items']:
-                camera_refs.append(camRespToRefs(item))
-            print(cam_resp['data']['nextPageExists'])
-            if cam_resp['data']['nextPageExists'] == True:
-                start += 100
-                limit += 100
-                time.sleep(1)
-            if cam_resp['data']['nextPageExists'] == False:
-                cameras_remaining == False
-                return camera_refs
+    return cameras
 
 def getCameraDetails(cUid,camName,objectType,vsomUid):
     getCamPayload = {"cameraRef":{"refUid":cUid,"refName":camName,"refObjectType":objectType,"refVsomUid":vsomUid}}
-    getCameraReq = s.post('https://{0}/ismserver/json/camera/v3_1/getCamera'.format(vsom_ip), json=getCamPayload, verify=False)
+    getCameraReq = s.post('https://{0}/ismserver/json/camera/v3_1/getCamera'.format(vsmIp), json=getCamPayload, verify=False)
     camResp = json.loads(getCameraReq.text)
     camDetail = camResp['data']
     return camDetail
 
-def checkRunningJobs():   #My intent with this function - if there are more than 10 queued jobs, I need to wait for that number to come down before running my below loop farther.
+def checkRunningJobs():   #My intent with this function - if there are more than 5 queued jobs, I need to wait for that number to come down before running my below loop farther.
     runningJobs = True
     while runningJobs == True:
         print("\n===========================================\n")
         print("Checking running jobs.")
-        getJobs = s.post('https://{0}/ismserver/json/job/v3_1/getAllJobCounts'.format(vsom_ip), verify=False)
+        getJobs = s.post('https://{0}/ismserver/json/job/v3_1/getAllJobCounts'.format(vsmIp), verify=False)
         jobsList = json.loads(getJobs.text)
         print("\nCurrent running jobs: {0}\nCurrent pending jobs: {1}".format(jobsList['data']['runningJobsCount'],jobsList['data']['pendingJobsCount']))
         if jobsList['data']['runningJobsCount'] >= 5:
@@ -116,57 +70,100 @@ def checkRunningJobs():   #My intent with this function - if there are more than
             runningJobs == False
             return
 
+
 def setCameraOverlay(camDetail):
     #print(camDetail)  #Prints camera payload before adding overlay specific parameters!
     overlayText = camDetail['name']
-    overlay = {"additionalCameraSettings":{"textOverlaySetting":{"overlayPlacement":"BOTTOM_OF_IMAGE","timeStampEnabled":True,"timeStampAlignment":"CENTER","textDisplayEnabled":True,"textAlignment":"CENTER","displayText":overlayText}}}
-
+    overlay = {"additionalCameraSettings":{"textOverlaySetting":{"overlayPlacement":overlay_top_bottom,"timeStampEnabled":True,"timeStampAlignment":time_stamp_alignment,"textDisplayEnabled":True,"textAlignment":text_alignment,"displayText":overlayText}}}
+    print(overlay)
     camDetail.update(overlay)
     setOverlayPayload = {"Device":camDetail,"addtlCameraActions":[]}
     #print("Printing the overlay payload we're gonna send: {0}".format(setOverlayPayload))
-    updateOverlayReq = s.post('https://{0}/ismserver/json/camera/v3_1/updateCamera'.format(vsom_ip), json=setOverlayPayload, verify=False)
+    updateOverlayReq = s.post('https://{0}/ismserver/json/camera/v3_1/updateCamera'.format(vsmIp), json=setOverlayPayload, verify=False)
     updateResp = json.loads(updateOverlayReq.text)
     if updateResp['status']['errorType'] == "SUCCESS":
         result = "Successfully updated {0}".format(camDetail['name'])
         print("\n========================={0}========================\n".format(result))
+        return True
     else:
         error =  "Something must have went wrong here."
         print(error)
-
+        return False
 
 def mainLoop():
     try:
+        #initialize counters
+        overlay_successful = 0
+        overlay_fail = 0
         print("To quit, just use ctrl+c!")
-        location = str(input("Enter exact location name: "))
-        try:
-            s.post('https://{0}/ismserver/json/authentication/login'.format(vsom_ip), json=loginPayload, verify=False)
-        except requests.exceptions.RequestException:
-            print("Failed to login, check your IP address.")
-            sys.exit()
-        camera_list = getCamByLocation(location)
-        if camera_list == "Failed":
-            print("Something broked-ed, we got no cameras!")
+        #prompt for location string
+        location = input("Enter exact location name: ")
+        #set up login payload for VSM.
+        loginPayload = {"username":vsmUsername,"password":vsmPassword}
+        #establish http session, login to VSM.
+        s.post('https://{0}/ismserver/json/authentication/login'.format(vsmIp), json=loginPayload, verify=False)
+        #Get our camera list - function defined above
+        cameras = getCamByLocation(location)
+        if cameras == False:
+            print("Aborted!")
+            return
+        if len(cameras) == 0:
+            print("Something is wrong, we got no cameras!")
         else:
-            for item in camera_list:
+            for item in cameras:
                 runningJobs = True
-                if item['operState'] != "ok":
-                    print(item)
-                    print("\n\n**********Camera {0} appears disabled. Or something.**************\n\n".format(item['camName']))
-                else:
-                    cam_detail = getCameraDetails(item['camUid'],item['camName'],item['objectType'],item['refVsomUid'])
-                    set_overlay = setCameraOverlay(cam_detail)
-                    checkRunningJobs()
-                    time.sleep(1)
+                cUid = item['uid']
+                vsomUid = item['vsomUid']
+                objectType = item['objectType']
+                camName = item['name']
+                camDetail = getCameraDetails(cUid, camName, objectType, vsomUid)
+                setOverlay = setCameraOverlay(camDetail)
+                if setOverlay == True:
+                    overlay_successful += 1
+                elif setOverlay == False:
+                    overlay_fail += 1
+                    #Now check running jobs.  I'm worried that VSOM really doesn't handle jobs well.  And I do not want to queue 500 of these jobs, as
+                    #any more than 5 running jobs puts VSOM in like a blocking state, all new jobs are queued.  Is a pain.
+                checkRunningJobs()
+                time.sleep(1)
+        print("\n\n\nSuccessfully applied overlay to {0} devices, while {1} failed.  Scroll up for details.\n\n\n\n\n\n".format(overlay_successful,overlay_fail))
     except KeyboardInterrupt:
-        print("  You pushed ctrl+c or some other madness! Bye!")
+        print("Got SIGTERM, exiting the program!")
         sys.exit()
 
-s = requests.Session()
-print("Some setup tasks first!!")
-vsom_ip = str(input("Please enter the VSOM IP Address: "))  #Didn't test, but want to make sure the IP entered is stored as a string, dont' want python to guess.
-vsmUsername = input("Please enter username: ")
-vsmPassword = input("Please enter the password: ")
-loginPayload = {"username":vsmUsername,"password":vsmPassword}
+if __name__ == "__main__":
+    #Set up requests session
+    s = requests.Session()
+    try:
+        overlay_verical = False
+        text_horizontal = False
+        time_horizontal = False
+        print("Some setup tasks first!!")
+        vsmIp = input("Please enter the VSOM IP Address: ")
+        vsmUsername = input("Please enter username: ")
+        vsmPassword = getpass.getpass()
+        while overlay_vertical = False:
+            overlay_top_bottom = input("Will the text overlay be at the TOP or BOTTOM of the image? ").upper()
+            if overlay_top_bottom != "TOP" or overlay_top_bottom != "BOTTOM":
+                print("Invalid input, please try again.  Valid input is either 'TOP' or 'BOTTOM'.")
+            else:
+                overlay_vertical = True
+        while text_horizontal == False:
+            text_alignment = input("Horizontal alignment for text? 'LEFT', 'RIGHT', or 'CENTER': ").upper()
+            if text_alignment != "LEFT" or text_alignment != "RIGHT" or text_alignment != "CENTER":
+                print("Please enter a valid option. 'LEFT', 'RIGHT' or 'CENTER'.")
+            else:
+                text_horizontal = True
+        while time_horizontal == False:
+            time_stamp_alignment = input("You know the drill by now, timespamp alignment. LEFT, RIGHT, CENTER: ").upper()
+            if time_stamp_alignment != "LEFT" or time_stamp_alignment != "RIGHT" or time_stamp_alignment != "CENTER":
+                print("Really man? Get in the game! LEFT, RIGHT or CENTER!")
+            else:
+                time_horizontal = True
+    except KeyboardInterrupt:
+        print("Got SIGTERM, exiting the program!")
+        sys.exit()
 
-while True:
-    mainLoop()
+    while True:
+        #infinitely loop our main function
+        mainLoop()
